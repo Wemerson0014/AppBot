4ec78adfdd062baae329c8fddc00ee11320c88e0
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-05-13 15:24:04 -0300

    Aplicativo funcionando para usuário especifico

---
+++ app/src/main/java/br/com/estudo/appbot/Action.kt
@@ -0,0 +1,34 @@
+package br.com.estudo.appbot
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
+
+class Action(private val action: NotificationCompat.Action) {
+
+    fun sendReply(context: Context, message : String) {
+        val intent = Intent()
+        val bundle = Bundle()
+        val remoteInputs = arrayListOf<RemoteInput>()
+
+        // Processo de envio da mensagem
+        if (action?.remoteInputs != null) {
+            for (input in action.remoteInputs) {
+                bundle.putCharSequence(input.resultKey, message)
+                    val newRemoteInput = RemoteInput.Builder(input.resultKey)
+                        .setLabel(input.label)
+                        .setChoices(input.choices)
+                        .setAllowFreeFormInput(input.allowFreeFormInput)
+                        .addExtras(input.extras)
+                        .build()
+
+                    remoteInputs.add(newRemoteInput)
+                }
+            }
+
+        RemoteInput.addResultsToIntent(remoteInputs.toTypedArray(), intent, bundle)
+        action.actionIntent.send(context, 0, intent)
+    }
+}
---
+++ app/src/main/java/br/com/estudo/appbot/NotifierListener.kt
@@ -0,0 +1,57 @@
+package br.com.estudo.appbot
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.os.AsyncTask
+import android.os.Bundle
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import java.lang.ref.WeakReference
+import java.util.*
+
+class NotifierListener : NotificationListenerService() {
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        println("Connected")
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+
+        val notification = sbn?.notification
+        val extras = notification?.extras
+
+        println(extras)
+        extras?.let {
+            //Evento de limpeza da Statusbar
+            this@NotifierListener.cancelNotification(sbn.key)
+
+            //Verifica se é o WhatsApp
+            var processName =
+                (it["android.rebuild.applicationInfo"] as? ApplicationInfo)?.processName
+
+            if (processName == null)
+                processName = (it["android.appInfo"] as? ApplicationInfo)?.processName
+
+            // Verifica o nome do contato e ignora resumo da mensagem e imagem recebida
+            if (processName == "com.whatsapp"
+                && it.getString(Notification.EXTRA_TITLE) == "Debora Cristina"
+                && it.getString(Notification.EXTRA_BIG_TEXT) == null
+                && it.getString(Notification.EXTRA_SUMMARY_TEXT) == null
+            ) {
+
+                //Aqui faz as respostas
+                val action = Notificationutils.quickReplyAction(sbn.notification)
+
+                try {
+                    action?.sendReply(applicationContext, "Olá Debora, aqui é um robô te respondendo!!!")
+                } catch (e: PendingIntent.CanceledException) {
+
+                }
+            }
+        }
+    }
+}
